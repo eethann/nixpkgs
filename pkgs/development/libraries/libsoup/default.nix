29819e798733afddd0fc51b08bf758287539bf0b
Jan Tojnar <jtojnar@gmail.com>
2018-09-11 12:38:55 +0200

    fixup! gnome3.libsoup: Fix meson build with upstream patches

--- pkgs/development/libraries/libsoup/default.nix
+++ pkgs/development/libraries/libsoup/default.nix
@@ -16,27 +16,27 @@ stdenv.mkDerivation rec {
     # Meson patches from upstream
     (fetchpatch {
       url = https://gitlab.gnome.org/GNOME/libsoup/commit/f134b372d34131d94a6aadd44f351e52a005f8d6.patch;
-      sha256 = "1xz12jah1d322p6qb08dii6md3rwnqgbifq4vyp7q3lf458c58l0";
+      sha256 = "1zd5mg7g4scyf5akvaygydi5w7r7x44n8cazl618c9k56fly55kb";
     })
     (fetchpatch {
       url = https://gitlab.gnome.org/GNOME/libsoup/commit/79526c35779ccfc932bda3c0ea3f05785b89a46a.patch;
-      sha256 = "0kyfia3bc043bj1nigwscci91qsi274f9di1phvp5pjh5ll50zm9";
+      sha256 = "1v8x4cxbhbsy2wp4ihzivafhpjlv6wcq7nr4nrh2p9chs5jc1dgj";
     })
     (fetchpatch {
       url = https://gitlab.gnome.org/GNOME/libsoup/commit/6dc3f241cfe11b31fdbccf65d35aac45c3aaf6bc.patch;
-      sha256 = "1yavh96ah3j8f21rf4qrb33gc0jais0x5gc5w3d5d0f1k2xa0912";
+      sha256 = "13gzrq76hhyjc09b6qv4q6xq2wsxf3gpdf02v0jn0ikf5flqb0ln";
     })
     (fetchpatch {
       url = https://gitlab.gnome.org/GNOME/libsoup/commit/dfaca0bd03a35426b629278067019dcc129a948d.patch;
-      sha256 = "0pg3rrx2qyficrzyp8wl7l7dy2qvawc0qyp8pqvd45j4s0kl4jc9";
+      sha256 = "1g10839ybmkjigr6bpx4irng5129nbgcgwzv9sgrbq6i1ndgk8k0";
     })
     (fetchpatch {
       url = https://gitlab.gnome.org/GNOME/libsoup/commit/2a7ff0ab0b81c3d4b8be217786b41b016fa56623.patch;
-      sha256 = "03r7xx9qcgf0w813fzq3nqc4ilkxcbgndk7n76jyjppcz52pkh00";
+      sha256 = "06sb7swzgmxqa4kjqh5a77x0wnl4dv590d999z1fjqiad88n5d55";
     })
     (fetchpatch {
       url = https://gitlab.gnome.org/GNOME/libsoup/commit/1dd3333a793089768506ad07fbf328ac13fd2897.patch;
-      sha256 = "1cyfw2s72hip5rkfsg7d216rvzg3rs7ff18bsf3h0wrnhf2dd66f";
+      sha256 = "1d5816sg808dnx0vfqpnprz3xx61f71q8wj6akrbljxczkwkv6g7";
     })
   ];
 
