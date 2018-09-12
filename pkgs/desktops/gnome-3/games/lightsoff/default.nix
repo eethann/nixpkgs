5d1478a2e51e1d14f457353a77932718c346e2ec
Tor Hedin Brønner <torhedinbronner@gmail.com>
2018-09-12 13:05:37 +0200

    fixup! gnome3.lightsoff: 3.28.0 → 3.30.0

--- pkgs/desktops/gnome-3/games/lightsoff/default.nix
+++ pkgs/desktops/gnome-3/games/lightsoff/default.nix
@@ -1,5 +1,6 @@
 { stdenv, fetchurl, vala, pkgconfig, gtk3, gnome3, gdk_pixbuf, librsvg, wrapGAppsHook
-, gettext, itstool, clutter, clutter-gtk, libxml2, appstream-glib }:
+, gettext, itstool, clutter, clutter-gtk, libxml2, appstream-glib
+, meson, ninja, python3 }:
 
 stdenv.mkDerivation rec {
   name = "lightsoff-${version}";
@@ -10,7 +11,16 @@ stdenv.mkDerivation rec {
     sha256 = "1cv5pkw0n8k5wb98ihx0z1z615w1wc09y884wk608wy40bgq46wp";
   };
 
-  nativeBuildInputs = [ vala pkgconfig wrapGAppsHook itstool gettext appstream-glib libxml2];
+  postPatch = ''
+    chmod +x meson_post_install.py # patchShebangs requires executable file
+    patchShebangs meson_post_install.py
+    sed -i '/gtk-update-icon-cache/s/^/#/' meson_post_install.py
+  '';
+
+  nativeBuildInputs = [
+    vala pkgconfig wrapGAppsHook itstool gettext appstream-glib libxml2
+    meson ninja python3
+  ];
   buildInputs = [ gtk3 gnome3.defaultIconTheme gdk_pixbuf librsvg clutter clutter-gtk ];
 
   enableParallelBuilding = true;
